56640064afbc086398da1d487b39bd686bfc803e
Powerm1nt <leonard.chabot@nuka.works>
2025-04-20 01:26:30 +0200

    Code Refactor

--- src/java.base/haiku/native/libnio/ch/FileDispatcherImpl.c
+++ src/java.base/haiku/native/libnio/ch/FileDispatcherImpl.c
@@ -31,7 +31,7 @@
 #include "jni.h"
 #include "nio.h"
 #include "nio_util.h"
-#include "sun_nio_ch_FileDispatcherImpl.h"
+#include "sun_nio_ch_UnixFileDispatcherImpl.h"
 
 static jlong
 handle(JNIEnv *env, jlong rv, char *msg)
@@ -44,8 +44,33 @@ handle(JNIEnv *env, jlong rv, char *msg)
     return IOS_THROWN;
 }
 
+JNIEXPORT jint JNICALL
+Java_sun_nio_ch_UnixFileDispatcherImpl_force0(JNIEnv *env, jobject this,
+                                          jobject fdo, jboolean md)
+{
+    jint fd = fdval(env, fdo);
+    int result = 0;
+
+    result = fcntl(fd, F_FULLFSYNC);
+    if (result == -1) {
+        struct statfs fbuf;
+        int errno_fcntl = errno;
+        if (fstatfs(fd, &fbuf) == 0) {
+            if ((fbuf.f_flags & MNT_LOCAL) == 0) {
+                /* Try fsync() in case file is not local. */
+                result = fsync(fd);
+            }
+        } else {
+            /* fstatfs() failed so restore errno from fcntl(). */
+            errno = errno_fcntl;
+        }
+    }
+
+    return handle(env, result, "Force failed");
+}
+
 JNIEXPORT jlong JNICALL
-Java_sun_nio_ch_FileDispatcherImpl_transferTo0(JNIEnv *env, jobject this,
+Java_sun_nio_ch_UnixFileDispatcherImpl_transferTo0(JNIEnv *env, jobject this,
                                                 jobject srcFDO,
                                                 jlong position, jlong count,
                                                 jobject dstFDO, jboolean append)
